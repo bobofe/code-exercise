75ab7e91c6ea9996231383e28a9c4865fd7b1e53
bobo <836050837@qq.com>
2019-04-13 22:00:23 +0800

    基本功能完成

---
+++ MVC/comment/comment.js
@@ -0,0 +1,57 @@
+(function () {
+    var data = {
+        notes : [1,2,3]
+    };
+
+    var view = {
+        init:function () {
+            var html ='';
+
+            control.getInitNotes().forEach(function (value) {
+                var text = '';
+                text += '<li>'+ value + '</li>';
+                html += text;
+            });
+            this.html = html;
+            this.render();
+            control.submitNewNote();
+        },
+
+        addNewNote : function () {
+            this.html += '<li>' + control.getNewNote() + '</li>';
+            this.render();
+        },
+
+        render : function () {
+            var ul = document.getElementById('notes');
+            ul.innerHTML = this.html;
+        }
+
+    };
+
+    var control = {
+        init : function () {
+            view.init()
+        },
+
+        getInitNotes : function () {
+            return data.notes;
+        },
+
+        getNewNote : function () {
+            var inputValue = document.getElementById('new-note-content').value;
+
+            return inputValue;
+        },
+
+        submitNewNote : function () {
+            var button = document.getElementById('submit');
+            button.addEventListener('click',function () {
+                view.addNewNote();
+            },false)
+        }
+
+    };
+
+    control.init()
+})(window);
